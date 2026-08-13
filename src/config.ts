4eb6dcb4e9dc9787d527a49f3ffea12ba1123d91
iwxh <kinghh826@gmail.com>
2026-08-12 19:27:59 -0700

    fix: prepare Astro blog for Cloudflare Pages

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://astro-blog.pages.dev/", // replace this when attaching a custom domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
